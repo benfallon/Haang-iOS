cbd6c1fff2f0f725c4c2f701d4fc47fa0a2445cc
Benjamin Fallon <benjamin.a.fallon@gmail.com>
2016-02-04 19:48:44 -0800

    Add editing mode

--- Haang/PostsViewController.swift
+++ Haang/PostsViewController.swift
@@ -12,13 +12,46 @@ class PostsViewController: UITableViewController {
     
     var postStore: PostStore!
     
+    @IBAction func addNewPost(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        //If you are currently in editing mode...
+        if editing {
+            //Change text of button to inform user of state
+            sender.setTitle("Edit", forState: .Normal)
+            
+            //Turn off editing mode
+            setEditing(false, animated: true)
+        }
+        else {
+            //Change text of button to inform user of state
+            sender.setTitle("Done", forState: .Normal)
+            
+            //Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return postStore.allPosts.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //Create an instance of UITableViewCell, with default appearance
-        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "UITableViewCell")
+        //Get a new or recycled cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         //Set the text on the cell with the name that is at the nth index of posts, where n = row this cell will appear in on the tableView
         let post = postStore.allPosts[indexPath.row]
